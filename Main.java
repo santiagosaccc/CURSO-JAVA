3c1d21be403028d5e352e85ea95e94478ebc4c6d
santiago ramos vargas <63522159+santiagosaccc@users.noreply.github.com>
2022-04-28 03:04:54 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+  //definimos la variable
+    int miVariableEntera = 10;
+    System.out.println(miVariableEntera);
+   //modificamos el valor de la vriable 
+     miVariableEntera = 5 ;
+    System.out.println(miVariableEntera);
+
+
+    String miVariableCadena = "saludos";
+      System.out.println(miVariableCadena);
+
+     miVariableCadena = "adios";
+      System.out.println(miVariableCadena);
+    
+    }
+}
